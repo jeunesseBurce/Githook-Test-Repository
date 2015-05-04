99e3bd88b9066a4388743893cd71327188128eb1
nesse <jdeburce@gmail.com>
2015-05-04 14:35:00 +0800

    Added shell script to fetch file from github

--- scripts/githook_test.sh
+++ scripts/githook_test.sh
@@ -1,1 +1,25 @@
 #This will serve as a shell script for githook on nodejs
+
+path="C:/Users/Jeunesse/Desktop/gith_test/Githook-Test-Repository"
+repository="https://github.com/jeunesseBurce/Githook-Test-Repository.git"
+path_zipfile = "C:/Users/Jeunesse/Desktop/gith_test/Githook-Test-Repository/master_repo.zip"
+
+echo "The path to where the zip file will be downloaded to is: "
+echo $path
+
+echo "The repository where the zip file will come from: "
+echo $repository
+
+# Get zip file from the repository
+cd  $path && wget -O master_repo.zip -q $repository
+
+# If zip file exists, unzip file 
+if [ -f $path_zipfile ]; then
+    
+    unzip -q $path_zipfile
+
+# After file is unzip, delete the zip file
+    
+    rm $path_zipfile
+
+fi
