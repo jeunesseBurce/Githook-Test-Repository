53a2cb98cd3d2f3d28965593f6a48eb2528a776d
nesse <jdeburce@gmail.com>
2015-05-07 17:22:43 +0800

    update Signed-off-by: nesse <jdeburce@gmail.com>

--- scripts/githook_test.sh
+++ scripts/githook_test.sh
@@ -5,6 +5,8 @@ repository="https://github.com/jeunesseBurce/Githook-Test-Repository.git"
 path_zipfile="C:/Users/Jeunesse/Desktop/gith_test/Githook-Test-Repository/master_repo.zip"
 local_folder = git_files
 
+
+
 echo "The path to where the zip file will be downloaded to is: "
 echo $path
 
@@ -13,6 +15,7 @@ echo $repository
 
 # Get zip file from the repository
 cd $local_folder && wget -O master_repo.zip $repository
+# Download from repository the zip file
 
 # If zip file exists, unzip file 
 #if [ -f $path_zipfile ]; then
