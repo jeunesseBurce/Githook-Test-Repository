245c60e682e371ad33516fe2fcbb94cbf7790b34
nesse <jdeburce@gmail.com>
2015-05-11 17:49:52 +0800

    WIP test Signed-off-by: nesse <jdeburce@gmail.com>

--- scripts/githook_test.sh
+++ scripts/githook_test.sh
@@ -1,28 +1,18 @@
 #This will serve as a shell script for githook on nodejs
-
 path="C:/Users/Jeunesse/Desktop/gith_test/Githook-Test-Repository/scripts/git_files"
-repository="https://github.com/jeunesseBurce/Githook-Test-Repository.git/master/server.js"
-path_zipfile="C:/Users/Jeunesse/Desktop/gith_test/Githook-Test-Repository/master_file.zip"
-local_folder = git_files
-
+repository="https://github.com/jeunesseBurce/Githook-Test-Repository.git"
+path_zipfile="C:/Users/Jeunesse/Desktop/gith_test/Githook-Test-Repository/gith_test.zip"
+path_repo="https://github.com/jeunesseBurce/Githook-Test-Repository/blob/master/gith_test.zip"
 echo "The path to where the zip file will be downloaded to is: "
 echo $path
-
 echo "The repository where the zip file will come from: "
 echo $repository
-
-
 # Get zip file from the repository
-cd $local_folder && wget -O master_repo.zip $file
+cd git_files && wget -O gith_test.zip $path_repo
 # Download from repository the zip file
-
 # If zip file exists, unzip file 
 #if [ -f $path_zipfile ]; then
-    
  #   unzip -q $path_zipfile
-
 # After file is unzip, delete the zip file
-    
   #  rm $path_zipfile
-
 #fi
